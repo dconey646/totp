623f49e0f24ffc7573af197fd675db932d7e6c6b
Damien Coney <d.coney@kainos.com>
2016-08-01 13:25:04 +0100

    Adding node and bower for matching library. Speakeasy library to be precise

--- pinGen.js
+++ pinGen.js
@@ -1,41 +1,51 @@
+var speakeasy = require('speakeasy');
 var $secret = $('#secret');
 var $drift = $('#drift');
 var $pin = $('#pin');
+var pins = [];
 var lastPayload = '';
 
 function update_pin() {
-  chrome.extension.getBackgroundPage().console.log("I got here!");
+  console.log("I got here!");
   var secret = $secret.val();
   if (secret == null || secret.length == 0) {
     return;
   }
-  $.post({
-    url: "/gen",
-    data: {'secret': secret, 'drift': $drift.val()}
-  }).success(function (data) {
-    chrome.extension.getBackgroundPage().console.log("I got here! success!");
-    currentPayload = JSON.stringify(data);
-    if (currentPayload == lastPayload) {
-      return;
-    }
-    lastPayload = currentPayload;
-    var source = $("#pin-template").html();
-    console.log(source);
-    var template = Handlebars.compile(source);
-    $('#pin-stack').html("");
-    data.forEach(function (el) {
-      $('#pin-stack').append(template(el))
-      new Clipboard('#copyToClipboard_' + el.driftIndex);
-    });
-  }).fail(function () {
-    chrome.extension.getBackgroundPage().console.log("I got here! Dammit.....");
-    $('#pin-stack').html('Invalid secret or server failure');
+
+  genPin(secret, drift);
+  currentPayload = JSON.stringify(data);
+  if (currentPayload == lastPayload) {
+    return;
+  }
+  lastPayload = currentPayload;
+  var source = $("#pin-template").html();
+  console.log(source);
+  $('#pin-stack').html("");
+  data.forEach(function (el) {
+    $('#pin-stack').append(template(el))
+    new Clipboard('#copyToClipboard_' + el.driftIndex);
+  });
+  $pin.val =
+  setInterval(function () {
+    update_pin();
+  }, 1000);
+  $secret.keyup(function () {
+    update_pin();
   });
 }
-setInterval(function () {
-  update_pin();
-}, 1000);
-$secret.keyup(function () {
-  update_pin();
-});
+function genPin(secret, drift){
+  var offset = i * windowSize;
+
+  console.log("I got here!");
+  var pin = speakeasy.totp({
+    secret: secret,
+    encoding: 'hex',
+    algorithm: 'sha1',
+    time : moment().add(offset, 'seconds').unix()
+  });
+  pins.push({ driftIndex : i, pin : pin, current : offset == 0});
+  console.log(pins);
+}
+
+
 document.addEventListener('DOMContentLoaded', update_pin);
