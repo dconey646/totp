c40b8447efde378b50a0187b03142e3a468c0ced
Damien Coney <d.coney@kainos.com>
2016-08-02 14:50:45 +0100

    Removing unnecessary code from Gulpfile. Updating HTML to remove the clipboard buttons as autofill now works.

--- app/src/pinGen.js
+++ app/src/pinGen.js
@@ -1,16 +1,18 @@
 $(document).ready(function () {
 
+  //Injecting moment and speakeasy to match previous implementation
   var moment = require('moment')
   var speakeasy = require('speakeasy');
+
   var $secret = $('#secret');
   var $drift = $('#drift');
   var $pin = $('#pin');
   var pins = [];
   var lastPayload = '';
   var windowSize = 30; // seconds
+  var $fillPinSignInButton = $('#fillPinSignInButton');
 
   function update_pin() {
-    console.log("I got here!");
     var secret = $secret.val();
     var drift = $drift.val();
     if (secret == null || secret.length == 0) {
@@ -19,12 +21,13 @@ $(document).ready(function () {
 
     genPin(secret, drift);
     currentPayload = JSON.stringify(pins);
-    //if (currentPayload == lastPayload) {
-      //return;
-    //}
+    if (currentPayload == lastPayload) {
+      return;
+    }
     lastPayload = currentPayload;
-    var lastPin = pins.length;
-    $pin.val(pins[lastPin - 1].pin);
+    $pin.val(pins[pins.length - 1].pin);
+
+    fillPinSignInButton.click();
   }
 
   setInterval(function () {
@@ -34,24 +37,29 @@ $(document).ready(function () {
     update_pin();
   });
 
-  $(document).getElementById("fillPinSignIn").addEventListener("click", function(){
-    var script = "";
+  document.getElementById('fillPinSignInButton').addEventListener("click", function(){
+    //if the active tab is the motdev site.
+    //Add in check to parent tab to ensure it is motdev site.
+    if(pins != null) {
+      var script = "document.getElementById('pin').value = " + pins[pins.length - 1].pin  + ";";
+      //var script = "console.log(document.getElementById('pin'));";
+    }
     //Fill in script to inject pin to motdev site
     chrome.tabs.executeScript({code : script});
   });
 
   function genPin(secret, drift){
-   for(var i = - drift ; i <= drift; ++i ) {
+    for(var i = - drift ; i <= drift; ++i ) {
 
-    var offset = i * windowSize;
+      var offset = i * windowSize;
 
-    var pin = speakeasy.totp({
-      secret: secret,
-      encoding: 'hex',
-      algorithm: 'sha1',
-      time : moment().add(offset, 'seconds').unix()
-    });
-    pins.push({ driftIndex : i, pin : pin, current : offset == 0});
+      var pin = speakeasy.totp({
+        secret: secret,
+        encoding: 'hex',
+        algorithm: 'sha1',
+        time : moment().add(offset, 'seconds').unix()
+      });
+      pins.push({ driftIndex : i, pin : pin, current : offset == 0});
+    }
   }
-}
 });
